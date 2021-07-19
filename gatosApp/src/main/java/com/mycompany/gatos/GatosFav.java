83a7b0c64e3cab28a988267604365f4ade333a26
Overlord <leon.pa.adolfo@gmail.com>
2021-07-19 09:31:33 -0500

    Se añade el codigo de gatosApp al repositorio

---
+++ gatosApp/src/main/java/com/mycompany/gatos/GatosFav.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.gatos;
+
+/**
+ *
+ * @author OVERLORD
+ */
+class GatosFav {
+    String id;
+    String image_id;
+    String apikey= "a47086db-0f7d-4a0a-8b42-10a0805cac44"; 
+    Imagex image;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getImage_id() {
+        return image_id;
+    }
+
+    public void setImage_id(String image_id) {
+        this.image_id = image_id;
+    }
+
+    public String getApikey() {
+        return apikey;
+    }
+
+    public void setApikey(String apikey) {
+        this.apikey = apikey;
+    }
+
+    public Imagex getImage() {
+        return image;
+    }
+
+    public void setImage(Imagex image) {
+        this.image = image;
+    }
+
+}
+
+
